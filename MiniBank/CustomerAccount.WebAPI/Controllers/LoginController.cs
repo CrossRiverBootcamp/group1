98e8ebb401ec7b203fcb259974fc9f59d3392a26
Miriam <Miri77331@users.noreply.github.com>
2022-09-04 13:54:42 +0300

    CREATE: CustomerAccount controllers

---
+++ MiniBank/CustomerAccount.WebAPI/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CustomerAccount.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+       
+        // POST api/<LoginController>
+        [HttpPost]
+        public Task<int> Post([FromBody] LoginDTO loginDTO)
+        {
+
+        }
+
+    }
+}
