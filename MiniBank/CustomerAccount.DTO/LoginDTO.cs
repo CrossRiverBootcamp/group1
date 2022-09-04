34e08f0b3ba236f1a2f7bad7b7b7a04dee0a8e8c
ayala greenfeld <mag0520757@gmail.com>
2022-09-04 13:41:25 +0300

    add DTO and AutoMapping

---
+++ MiniBank/CustomerAccount.DTO/LoginDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerAccount.DTO
+{
+    public class LoginDTO
+    {
+        [Required]
+        [EmailAddress]
+        [MaxLength(25)]
+        public string Email { get; set; }
+        [Required]
+        [MaxLength(15)]
+        [MinLength(4)]
+        public string Password { get; set; }
+    }
+}
