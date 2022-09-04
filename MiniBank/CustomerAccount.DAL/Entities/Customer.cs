5249d4ab22acbb4e527576cbc486d3027849c5cc
r1396 <r0548501396@gmail.com>
2022-09-04 16:34:02 +0300

    Created Dal layer, Entities and Ef

---
+++ MiniBank/CustomerAccount.DAL/Entities/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerAccount.DAL.Entities
+{
+    [Index("Email", IsUnique = true)]
+    public class Customer
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        [Required]
+        [MaxLength(25)]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(25)]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        [MaxLength(25)]
+        public string Email { get; set; }
+        [Required]
+        [MaxLength(15)]
+        public string Password { get; set; }
+
+    }
+}
--- MiniBank/CustomerAccount.WebAPI/Program.cs
+++ MiniBank/CustomerAccount.WebAPI/Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using CustomerAccount.DAL.EF;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContextFactory<CustomerAccountDBContext>(item =>
+    item.UseSqlServer(builder.Configuration.GetConnectionString("myconn"))); 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
