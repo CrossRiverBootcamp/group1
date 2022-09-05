ee55ad5dc31824388f43f362e43772a943a0da84
r1396 <r0548501396@gmail.com>
2022-09-05 17:49:23 +0300

    client: added services

--- MiniBankUI/src/app/models/customer-account.model.ts
+++ MiniBankUI/src/app/models/customer-account.model.ts
@@ -1,14 +1,6 @@
 export interface CustomerAccount {
-  firstName:string;
-  lastName :string;
-  email:string;
-  password:string;
-  openDate:Date;
-  Balance:number;
-}
-export interface CustomerAccountInfo {
-  firstName:string;
-  lastName :string;
-  openDate:Date;
-  Balance:number;
-}
+    firstName:string;
+    lastName :string;
+    openDate:Date;
+    Balance:number;
+  }
