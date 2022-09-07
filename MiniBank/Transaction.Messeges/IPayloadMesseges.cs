e41bc8c346c5552d3d981af0a2e50412261abb55
ayala greenfeld <mag0520757@gmail.com>
2022-09-07 17:34:00 +0300

    FIX: space in ipayload interface

--- MiniBank/Transaction.Messeges/IPayloadMesseges.cs
+++ MiniBank/Transaction.Messeges/IPayloadMesseges.cs
@@ -2,19 +2,13 @@
 {
     public interface IPayloadMesseges
     {
-      
         public Guid TransactionId { get; set; }
 
-      
         public Guid FromAccountId { get; set; }
 
-       
         public Guid ToAccountId { get; set; }
 
-       
         public int Amount { get; set; }
-
- 
 
     }
 }
