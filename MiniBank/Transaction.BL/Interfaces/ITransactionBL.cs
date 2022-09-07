4ebe8fb5d1bf6886ac6e2d71df7650ffefd2537f
r1396 <r0548501396@gmail.com>
2022-09-07 22:19:09 +0300

    testind

---
+++ MiniBank/Transaction.BL/Interfaces/ITransactionBL.cs
@@ -0,0 +1,10 @@
+﻿using Transaction.DTO;
+
+namespace Transaction.BL.Interfaces
+{
+    public interface ITransactionBL
+    {
+        Task<bool> PostTransactionStartSaga(TransactionDTO TransactionDTO);
+        Task ChangeTransactionStatus(UpadateTransactionStatusDTO upadateTransactionStatusDTO);
+    }
+}
---
+++ MiniBank/Transaction.DAL/Models/StatusEnum.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transaction.DAL.Models;
+public enum StatusEnum
+{
+    PROCESSING,SUCCESS,FAIL
+}
