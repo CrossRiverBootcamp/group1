bb6bf48c1fedb395e63a07479567aee7ca41d064
ayala greenfeld <mag0520757@gmail.com>
2022-09-07 16:55:10 +0300

    ADD : messeges classes

---
+++ MiniBank/Transaction.Messeges/TransactionMade.cs
@@ -0,0 +1,16 @@
+﻿using NServiceBus;
+
+namespace Transaction.Messeges
+{
+    public class TransactionMade : IEvent
+    {
+        public Guid TransactionId { get; set; }
+
+        public Guid FromAccountId { get; set; }
+
+        public Guid ToAccountId { get; set; }
+
+        public int Amount { get; set; }
+
+    }
+}
