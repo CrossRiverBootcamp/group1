ed13b6ff529ae26c003bc184f5decd2d9ed92528
Miriam <Miri77331@users.noreply.github.com>
2022-09-08 00:58:30 +0300

    add migration

---
+++ MiniBank/CustomerAccount.DAL/Migrations/20220907215608_change amount type to int type.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CustomerAccount.DAL.Migrations
+{
+    public partial class changeamounttypetointtype : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Balance",
+                table: "AccountDatas",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Balance",
+                table: "AccountDatas",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
