3154a537b8748db2f31455b24a532c3ad2e9168b
Miriam <Miri77331@users.noreply.github.com>
2022-09-08 17:39:23 +0300

    feat: transaction action completed

--- MiniBankUI/src/app/account/account-routing.module.ts
+++ MiniBankUI/src/app/account/account-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AccountDetailsComponent } from './account-details/account-details.component';
 import { LoginComponent } from './login/login.component';
 import { OpenAccountComponent } from './open-account/open-account.component';
+import { TransactionComponent } from './transaction/transaction.component';
 
 const routes: Routes =
 [
   {path:'',redirectTo:'login',pathMatch:'full'},
   {path:'open-account',component:OpenAccountComponent},
   {path:'account-details',component:AccountDetailsComponent},
+  {path:'transaction',component:TransactionComponent},
   {path:'login',component:LoginComponent}];
 
 @NgModule({
