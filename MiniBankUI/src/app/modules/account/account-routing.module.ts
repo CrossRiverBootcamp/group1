b5d31749e095ea718873f20dff30198956f90f33
Miriam <Miri77331@users.noreply.github.com>
2022-09-18 19:18:29 +0300

    feat: canActivate: [AuthGuard] added to impotnt routs

--- MiniBankUI/src/app/modules/account/account-routing.module.ts
+++ MiniBankUI/src/app/modules/account/account-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from 'src/app/guards/auth-guard.service';
 import { AccountDetailsComponent } from './account-details/account-details.component';
 import { ActionsComponent } from './actions/actions.component';
 import { OpenAccountComponent } from './open-account/open-account.component';
@@ -8,9 +9,9 @@ import { OpenAccountComponent } from './open-account/open-account.component';
 const routes: Routes =
 [
   {path:'open-account',component:OpenAccountComponent},
-  {path:'actions',component:ActionsComponent,loadChildren:()=>import('../operations/operations-routing.module')
+  {path:'actions',component:ActionsComponent, canActivate: [AuthGuard],loadChildren:()=>import('../operations/operations-routing.module')
   .then(m=>m.OperationsRoutingModule)},
-  {path:'account-details',component:AccountDetailsComponent},
+  {path:'account-details', canActivate: [AuthGuard],component:AccountDetailsComponent},
 ];
 
 @NgModule({
