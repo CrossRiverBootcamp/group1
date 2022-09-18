9c5ced40ab11ecf699a2667176dcc25fd7e661ab
r1396 <r0548501396@gmail.com>
2022-09-18 23:26:11 +0300

    small changes- and that's it from meee

--- MiniBankUI/src/app/modules/account/open-account/open-account.component.ts
+++ MiniBankUI/src/app/modules/account/open-account/open-account.component.ts
@@ -6,7 +6,7 @@ import { Customer } from 'src/app/models/customer.model';
 import { CustomerAccountService } from '../../../services/customer-account.service';
 import { EmailVerificationService } from '../../../services/email-verification.service';
 
-const ATTEMPTS_ALLOWED=4;
+const ATTEMPTS_ALLOWED = 4;
 //const NUM_OF_VERIFY_CODES_ALLOWED=2;
 
 
@@ -18,23 +18,23 @@ const ATTEMPTS_ALLOWED=4;
 export class OpenAccountComponent implements OnInit {
 
   accountDetailsForm!: FormGroup;
-  verificationForm!:FormGroup;
-  accountDetailsViewForm!:FormGroup;
+  verificationForm!: FormGroup;
+  accountDetailsViewForm!: FormGroup;
   loading = false;
   accountDetailsFormSubmitted = false;
   // verificationFormSubmitted=false;
 
-  userVerified:boolean=true;
-  emailExists: boolean=false;
-  numOfAttempts:number=0;
-  NumOfVerificationCodesSent:number=0;
+  userVerified: boolean = true;
+  emailExists: boolean = false;
+  numOfAttempts: number = 0;
+  NumOfVerificationCodesSent: number = 0;
 
   constructor(
     private formBuilder: FormBuilder,
     private router: Router,
     private accountService: CustomerAccountService,
     private emailVerificationService: EmailVerificationService
-   // private alertService: AlertService
+    // private alertService: AlertService
   ) { }
 
   ngOnInit() {
@@ -45,13 +45,13 @@ export class OpenAccountComponent implements OnInit {
       lastName: ['', Validators.required],
     });
 
-    this.verificationForm=this.formBuilder.group({
-      verificationCode:['',[Validators.required
+    this.verificationForm = this.formBuilder.group({
+      verificationCode: ['', [Validators.required
         // , Validators.pattern("[0-9]{6}")
       ]]
     })
 
-    this.accountDetailsViewForm=this.formBuilder.group({});
+    this.accountDetailsViewForm = this.formBuilder.group({});
 
   }
 
@@ -61,55 +61,51 @@ export class OpenAccountComponent implements OnInit {
   // convenience getter for easy access to verificationForm fields
   get f2() { return this.verificationForm.controls; }
 
-  getVerificationCodeByEmail(isResend:boolean){
+  getVerificationCodeByEmail(isResend: boolean) {
     if (this.accountDetailsForm.invalid) {
       return;
     }
-    this.accountDetailsFormSubmitted=true;
+    this.accountDetailsFormSubmitted = true;
     this.accountDetailsForm.disable();
     //this.NumOfVerificationCodesSent++;
-    this.emailVerificationService.sendEmailVerification(this.f['email'].value,isResend)
-      .subscribe(()=>{
+    this.emailVerificationService.sendEmailVerification(this.f['email'].value, isResend)
+      .subscribe(() => {
         alert("verification code sent. check you email")
       },
-      (error)=>{
-        alert("can not resend a code... sorry:(")
-        window.location.reload()
-      }
+        (error) => {
+          alert("can not resend a code... sorry:(")
+          window.location.reload()
+        }
       );
   }
 
-  checkIfEmailExists(){
-    this.emailExists=false;
-    if(this.f['email'].invalid)
-      {
-        return;
-      }
+  checkIfEmailExists() {
+    this.emailExists = false;
+    if (this.f['email'].invalid) {
+      return;
+    }
     this.accountService.customerExists(this.accountDetailsForm.get('email')?.value)
-    .subscribe((res:boolean)=>{
-      if(res)
-        {
-          this.emailExists=true;
+      .subscribe((res: boolean) => {
+        if (res) {
+          this.emailExists = true;
         }
-    })
+      })
   }
 
-  OpenNewAccount(){
-  this.loading = true;
-  let customer=this.accountDetailsForm.value as Customer;
-  customer.verificationCode=this.verificationForm.get('verificationCode')?.value;
+  OpenNewAccount() {
+    this.loading = true;
+    let customer = this.accountDetailsForm.value as Customer;
+    customer.verificationCode = this.verificationForm.get('verificationCode')?.value;
     this.accountService.createCustomerAccount(customer)
       .subscribe(
         (isAdded: boolean) => {
-          if(isAdded)
-          {
+          if (isAdded) {
             alert("Welcome!! please login");
-            this.router.navigateByUrl('account/login');
+            this.router.navigateByUrl('login');
           }
-          else
-          {
+          else {
             alert("ooops error acured, please try again");
-            this.router.navigateByUrl('account/login');
+            window.location.reload()
           }
           this.loading = false;
           //this.alertService.success('Registration successful', { keepAfterRouteChange: true });
@@ -119,18 +115,23 @@ export class OpenAccountComponent implements OnInit {
           switch (error.status) {
             case 401:
               {
-                  {alert(`wrong code. you have ${ATTEMPTS_ALLOWED-(this.numOfAttempts++)} attempts left`);
-                  this.verificationForm.get('verificationCode')?.setValue('88888');}
+                {
+                  alert(`wrong code. you have ${ATTEMPTS_ALLOWED - (this.numOfAttempts++)} attempts left`);
+                }
               }
               break;
-              case 417:alert('code expired. Try a resend requst');
+            case 417: alert('code expired. Try a resend requst');
               break;
-              case 429:{
-                alert(`wrong code. no more attempts`)
-                this.router.navigateByUrl('account/login');
+            case 429: {
+              alert(`wrong code. no more attempts`)
+              window.location.reload()
+            }
+              break;
+            default:
+              {
+                alert('Unresolved error:( Please try again later..');
+                window.location.reload()
               }
-              break;
-              default:alert('Unresolved error:( Please try again later..');
           }
           console.log(error);
           //this.alertService.error(error.message);
