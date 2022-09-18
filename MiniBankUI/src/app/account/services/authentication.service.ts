a9f306cd30b378c65fd3ce4e45ad55c68d2758c2
Miriam <Miri77331@users.noreply.github.com>
2022-09-18 12:10:49 +0300

    login service changed to auth service

--- MiniBankUI/src/app/account/services/authentication.service.ts
+++ MiniBankUI/src/app/account/services/authentication.service.ts
@@ -10,26 +10,16 @@ import { map } from 'rxjs/operators'
 
 export class AuthenticationService  {
 
-  private currentUserSubject: BehaviorSubject<User>;
-  public currentUser: Observable<User>;
+  private currentUserSubject: BehaviorSubject<LoginReturned>;
+  public currentUser: Observable<LoginReturned>;
 
   constructor(private http: HttpClient) {
-      this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
+      this.currentUserSubject = new BehaviorSubject<LoginReturned>(JSON.parse(localStorage.getItem('currentUser')||''));
       this.currentUser = this.currentUserSubject.asObservable();
   }
 
-  public get currentUserValue(): User {
+  public get currentUserValue(): LoginReturned {
       return this.currentUserSubject.value;
-  }
-
-  login(username: string, password: string) {
-      return this.http.post<any>(`${environment.apiUrl}/users/authenticate`, { username, password })
-          .pipe(map(user => {
-              // store user details and jwt token in local storage to keep user logged in between page refreshes
-              localStorage.setItem('currentUser', JSON.stringify(user));
-              this.currentUserSubject.next(user);
-              return user;
-          }));
   }
 
   logout() {
@@ -39,11 +29,11 @@ export class AuthenticationService  {
   }
 
   login(login:Login): Observable<string> {
-    return this._http.post<string>("api/Login",login).pipe(
-      map((accountId: string) => {
-        this.isUserLoggedIn = true;
-        this.accountId = accountId;
-        return accountId;
+    return this.http.post<string>("api/Login",login).pipe(
+      map((login: LoginReturned) => {
+        localStorage.setItem('currentUser', JSON.stringify(login));
+        this.currentUserSubject.next(login);
+        return login;
       })
     );
   }
